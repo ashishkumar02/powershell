2ac0a359745ff0abe398db0f2879691d2ddee29d
Ravi Kaza <ravi.kaza@equifax.com>
2020-09-24 12:43:02 +1000

    Test

--- workspace.ps1
+++ workspace.ps1
@@ -1,4 +1,4 @@
 Invoke-Command -ComputerName apprwidc121 -Credential $cred -ScriptBlock`
 {$fqdn=(Get-WmiObject win32_computersystem).DNSHostName+"."+(Get-WmiObject win32_computersystem).Domain;`
 get-childitem -Path cert:\localmachine\my -Recurse | Where-Object {$_.Subject -like "$fdqn"}}
-# wmic /namespace:\\root\cimv2\TerminalServices PATH Win32_TSGeneralSetting Set SSLCertificateSHA1Hash=$thumb}
+wmic /namespace:\\root\cimv2\TerminalServices PATH Win32_TSGeneralSetting Set SSLCertificateSHA1Hash=$thumb}
