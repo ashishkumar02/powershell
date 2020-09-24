771ed51264d035b6b0eecdb688ebb75c7e5160a0
ravikaza20 <70553469+ravikaza20@users.noreply.github.com>
2020-09-24 14:55:00 +1000

    Update workspace.ps1

--- workspace.ps1
+++ workspace.ps1
@@ -1,4 +1,4 @@
-Invoke-Command -ComputerName apprwidc121 -Credential $cred -ScriptBlock`
+Invoke-Command -ComputerName -Credential $cred -ScriptBlock`
 {$fqdn=(Get-WmiObject win32_computersystem).DNSHostName+"."+(Get-WmiObject win32_computersystem).Domain;`
 get-childitem -Path cert:\localmachine\my -Recurse | Where-Object {$_.Subject -like "$fdqn"}}
 wmic /namespace:\\root\cimv2\TerminalServices PATH Win32_TSGeneralSetting Set SSLCertificateSHA1Hash=$thumb}
