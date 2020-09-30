9c956256f4f569971cd9f3f2e646a3893542762b
unknown <Ravi Kaza>
2020-09-30 15:52:48 +1000

    removed files left one file

--- README.md
+++
@@ -1,3 +0,0 @@
-# powershell
-
-I am trying to test few things in GIt HUB no idea what its all about 
--- workspace.ps1
+++ workspace.ps1
@@ -1,5 +1,1 @@
-Invoke-Command -ComputerName -Credential $cred -ScriptBlock`
-{$fqdn=(Get-WmiObject win32_computersystem).DNSHostName+"."+(Get-WmiObject win32_computersystem).Domain;`
-get-childitem -Path cert:\localmachine\my -Recurse | Where-Object {$_.Subject -like "$fdqn"}}
-wmic /namespace:\\root\cimv2\TerminalServices PATH Win32_TSGeneralSetting Set SSLCertificateSHA1Hash=$thumb}
-New File name like 
+get-service
