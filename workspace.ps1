2d49b6217e6696544e78525add920a58f76de6d7
ashishkumar02 <66670904+ashishkumar02@users.noreply.github.com>
2020-10-01 11:35:05 +0530

    Added more action and cmdlets

--- workspace.ps1
+++ workspace.ps1
@@ -1,1 +1,8 @@
+##Get the services on the local computer.
 get-service
+##Get all the services on the remote computer Server64:
+get-service -computername Server64
+
+#To retrieve the description of a service use the Win32_Service WMI class:
+
+get-wmiobject win32_service | where-object {$_.Name -eq 'Schedule'} | format-list Name, Description
